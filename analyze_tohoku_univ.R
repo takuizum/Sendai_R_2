b0128b67cd098eaad5f6d4fbf58a66b5c8df797e
takuizum <sep10.taku.izum@gmail.com>
2019-10-02 00:31:59 +0900

    add analysis focus on year and month.

--- analyze_tohoku_univ.R
+++ analyze_tohoku_univ.R
@@ -26,12 +26,31 @@ View(tohoku_uni_r1)
 tohoku_uni_r1 %>% mutate(department = )
 
 # Look over a whole of data
+
+# The difficulty histogram filled by the attendance
 tohoku_uni_r1 %>% 
-  group_by(Difficulty = fct_explicit_na(Difficulty, )) %>% 
+  group_by(Difficulty = fct_explicit_na(Difficulty), Attendance = fct_explicit_na(Attendance)) %>% 
   summarise(count = n()) %>% 
   ggplot()+
-  geom_bar(aes(x = Difficulty, y = count, fill = ), stat = 'identity')+
+  geom_bar(aes(x = Difficulty, y = count, fill = Attendance), stat = 'identity')+
   theme_bw(base_family = "HiraKakuPro-W3")
+
+# Focus on the year.
+library(lubridate)
+# trace line
+tohoku_uni_r1 %>% 
+  group_by(Date) %>% 
+  summarize(count = n()) %>% 
+  ggplot(aes(x = Date, y = count))+
+  geom_line()
+# cumlative bar plot
+tohoku_uni_r1 %>% 
+  mutate(year = year(Date), month = month(Date) %>% factor(levels = c(4:12, 1:3))) %>% 
+  group_by(year, month = fct_explicit_na(month)) %>% 
+  summarize(count = n()) %>% 
+  ggplot(aes(x = year, y = count, group = month, fill = month))+
+  geom_bar(stat = 'identity')
+
   
 # Slice records which is relevant to pedagogy
 tohoku_uni_r1 %>% filter(Class %>% str_detect('教育')) %>% .$Teacher %>% unique
